5561952ec68d13e295a64e5b9b4f73859cd7ef9c
YY-Huang <yansquaredh@gmail.com>
2020-02-10 14:00:36 -0500

    anagram 1

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,35 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function createCharMap(str) {
+	const charMap = {};
+    
+	for (let char of str.replace(/[^\w]/g, '').toLowerCase()) { // not 0-9, a-z, A-Z
+		charMap[char] = charMap[char] + 1 || 1;
+	}
+    
+	return charMap;
+}
+
+function anagrams(stringA, stringB) {
+
+	// Method 1
+	/* const aCharMap = createCharMap(stringA);
+	const bCharMap = createCharMap(stringB);
+
+	if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+		return false;
+	}
+    
+	for (let char in aCharMap) {
+		if (aCharMap[char] !== bCharMap[char]) {
+			return false;
+		}
+	}
+    return true; */
+    
+	//Method 2
+    
+}
 
 module.exports = anagrams;
