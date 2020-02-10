f3024b3295959c7d5a5e71e1d62a435d321ea9d3
YY-Huang <yansquaredh@gmail.com>
2020-02-10 13:29:39 -0500

    chunk 2

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -24,7 +24,14 @@ function chunk(array, size) {
 	}
     return chunked; */
     
-    
+	//Method 2
+	const chunked = [];
+	let index = 0;
+	while (index < array.length) {
+		chunked.push(array.slice(index, index+size));
+		index+= size;
+	}
+	return chunked;
 }
 
 module.exports = chunk;
