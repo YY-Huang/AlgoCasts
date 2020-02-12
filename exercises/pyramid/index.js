5a36fb29b256b90e97d49fb8d284196eac7c4e87
YY-Huang <yansquaredh@gmail.com>
2020-02-12 13:17:09 -0500

    pyramid 1

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -5,15 +5,28 @@
 // pyramid has spaces on both the left *and* right hand sides
 // --- Examples
 //   pyramid(1)
-//       '#'
+//       '#' midpoint: 0
 //   pyramid(2)
-//       ' # '
+//       ' # ' midpoint: 1
 //       '###'
 //   pyramid(3)
-//       '  #  '
-//       ' ### '
-//       '#####'
+//       '  #  ' midpoint: 2, i = 0, j = 3
+//       ' ### ' i = 1, j = 3, 4, 5
+//       '#####' i = 2, j = 1, 2, 3, 4, 5
 
-function pyramid(n) {}
+function pyramid(n) {
+	const midpoint = Math.floor((2*n-1) / 2);
+	for (let i = 0; i < n; i++) {
+		let row = '';
+		for (let j = 0; j < 2 * n - 1; j++) {
+			if (midpoint - i <= j && midpoint + i >= j) {
+				row += '#';
+			} else {
+				row += ' ';
+			}
+		}
+		console.log(row);
+	}
+}
 
 module.exports = pyramid;
