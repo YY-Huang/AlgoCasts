07aef74587ba8399369b1b1e849fdb9df62a073a
YY-Huang <yansquaredh@gmail.com>
2020-02-28 12:15:53 -0500

    merge 1

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -36,7 +36,17 @@ function mergeSort(arr) {
 }
 
 function merge(left, right) {
+	const results = [];
+    
+	while (left.length && right.length) {
+		if (left[0] < right[0]) {
+			result.push(left.shift());
+		} else {
+			result.push(right.shift());
+		}
+	}
 
+	return [...results, ...left, ...right];
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort };
